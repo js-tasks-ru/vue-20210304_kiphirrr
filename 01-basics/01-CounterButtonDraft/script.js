963ab24ebbd03883a33eaa2e3d662c02811e20d1
Lobkovsky Kirill <kiphirrr@gmail.com>
2021-03-18 19:44:42 +0300

    01-01 & 01-02 solutions

--- 01-basics/01-CounterButtonDraft/script.js
+++ 01-basics/01-CounterButtonDraft/script.js
@@ -1,9 +1,15 @@
 import Vue from './vendor/vue.esm.browser.js';
 
 const app = new Vue({
+  data() {
+    return {
+      count: 0
+    }
+  },
+
   methods: {
-    incValue(event) {
-      event.target.innerHTML++;
+    incCount(event) {
+      this.count++;
     }
   }
 });
